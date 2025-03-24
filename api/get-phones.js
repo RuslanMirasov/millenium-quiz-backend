ff350dcf7fa775e6bc3f8269bad2762547a113d0
Ruslan <info@mirasov.dev>
2025-03-24 22:28:12 +0100

    fix cors errors

--- api/get-phones.js
+++ api/get-phones.js
@@ -5,8 +5,22 @@ dotenv.config();
 const client = new MongoClient(process.env.MONGODB_URI);
 const dbName = process.env.MONGODB_NAME;
 const collectionName = process.env.MONGODB_COLLECTION;
+const allowedOrigins = ['http://127.0.0.1:5500', 'https://mirasov.dev', 'https://milleniumstyle.ru'];
 
 export default async function handler(req, res) {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'GET') {
     return res.status(405).end('Method Not Allowed');
   }
@@ -18,9 +32,9 @@ export default async function handler(req, res) {
 
     const phones = await collection.find({}, { projection: { _id: 0 } }).toArray();
 
-    res.status(200).json({ success: true, phones });
-  } catch (err) {
-    console.error(err);
+    res.status(200).json({ phones });
+  } catch (error) {
+    console.error(error);
     res.status(500).json({ success: false, message: 'Ошибка сервера' });
   } finally {
     await client.close();
--- api/save-phone.js
+++ api/save-phone.js
@@ -5,8 +5,26 @@ dotenv.config();
 const client = new MongoClient(process.env.MONGODB_URI);
 const dbName = process.env.MONGODB_NAME;
 const collectionName = process.env.MONGODB_COLLECTION;
+const allowedOrigins = ['http://127.0.0.1:5500', 'https://mirasov.dev', 'https://milleniumstyle.ru'];
 
 export default async function handler(req, res) {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method !== 'POST') {
+    return res.status(405).end('Method Not Allowed');
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).end('Method Not Allowed');
   }
